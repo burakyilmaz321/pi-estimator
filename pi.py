61929a8288fa9ffd9e28568ec92c95381491c26a
Burak Yılmaz <burakyilmaz321@gmail.com>
2017-03-08 15:14:48 +0300

    update pi estimator

--- pi.py
+++ pi.py
@@ -1,28 +1,14 @@
 import numpy as np
 
-x_range = np.arange(-1., 1., 0.01)
-y_range = np.arange(-1., 1., 0.01)
+def estimate(n=1000):
+	random = np.random.rand(2*n).reshape(n, 2)
 
-inCircle = []
-points = []
-circle = []
-random = []
+	inCircle = []
 
-for x in x_range:
-	for y in y_range:
-		points.append((round(x, 2), round(y, 2)))
+	for r in random:
+		if r[0]**2 + r[1]**2 <= 1:
+			inCircle.append(r)
 
-for p in points:
-	if p[0]**2 + p[1]**2 <= 1:
-		circle.append(p)
-
-for i in range(10000):
-	random.append((round(np.random.random(), 2), round(np.random.random(), 2)))
-
-for c in random:
-	if c in circle:
-		inCircle.append(c)
-
-pi = float(len(inCircle)) / 10000. * 4
-
-print "Estimated Pi is: " + str(pi)
+	pi = float(len(inCircle)) / float(n) * 4
+	
+	print "Estimated Pi is: " + str(pi)
