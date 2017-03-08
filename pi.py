7673eb4bc00c9aaf412889c5d94b5749828ccf7c
Burak Yılmaz <burakyilmaz321@gmail.com>
2017-03-08 15:58:02 +0300

    make it object oriented, add notebook

--- pi.py
+++ pi.py
@@ -1,18 +1,21 @@
 import numpy as np
 
-def estimate(n=1000):
+class Pi(object):
 	"""
-	Estimates Pi using Monte Carlo Metho
-	Takes an integer n: number of random points as input
-	Outputs an estimaton of pi
+	Estimates Pi using Monte Carlo Method
+	Takes an integer points: number of random points as input
 	"""
-	random = np.random.rand(2*n).reshape(n, 2)
+	def __init__(self, points=1000):
+		self.points = points
 
-	inCircle = []
-
-	for r in random:
-		if r[0]**2 + r[1]**2 <= 1:
-			inCircle.append(r)
-
-	return float(len(inCircle)) / float(n) * 4
+	def estimate(self):
+		random = np.random.rand(2*self.points).reshape(self.points, 2)
+		inCircle = []
+		for r in random:
+			if r[0]**2 + r[1]**2 <= 1:
+				inCircle.append(r)
+		return float(len(inCircle)) / float(self.points) * 4
 	
+	def __str__(self):
+		pi = self.estimate()
+		return "Estimated Pi is: " + str(pi)
